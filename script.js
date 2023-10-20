d7467e280283f1773c54670f30ece04fd36ba204
thorcamara <thorcamaraalthoff@gmail.com>
2023-10-20 15:20:20 -0300

    Added code to Scroll Reveal Animation

--- script.js
+++ script.js
@@ -70,3 +70,19 @@ themeButton.addEventListener('click', () => {
   localStorage.setItem('selected-theme', getCurrentTheme())
   localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 2000,
+  reset: true
+});
+
+sr.reveal(`.home__data, .home__img,
+          .about__data, .about__img,
+          .services__content, .menu__content,
+          .app__data, .app__img,
+          .contact__data, .contact__button,
+          .footer__content`, {
+  interval: 200
+})
