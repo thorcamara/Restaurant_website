e693094495ed32ab1dcb6a4eb944e63e765c36dc
thorcamara <thorcamaraalthoff@gmail.com>
2023-10-20 15:19:02 -0300

    Added code to Show Scroll Top

--- script.js
+++ script.js
@@ -42,3 +42,9 @@ function scrollHeader() {
   if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top');
+  if (this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
