8a1751d5f138f8b1a9776ac7a496967fd1e3377a
thorcamara <thorcamaraalthoff@gmail.com>
2023-10-20 15:16:58 -0300

    Added code for Remove Menu Mobile

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ const showMenu = (toggleId, navId) => {
   }
 }
 showMenu('nav-toggle', 'nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
